a7fca7c93016ae11b8407f95a8bdaabb04aab1fa
Giordan Arredondo <garredondop@uni.pe>
2022-06-27 11:31:40 -0500

    configuration init

---
+++ src/main/java/com/examen/demoAFP/DemoAfpApplication.java
@@ -0,0 +1,13 @@
+package com.examen.demoAFP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAfpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAfpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/examen/demoAFP/DemoAfpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examen.demoAFP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAfpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
